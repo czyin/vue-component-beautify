141afecc3378e982cc1c7bdbd058e06f577c883e
czyin <chengzhi.yin@outlook.com>
2017-03-06 17:47:51 +0800

    init

---
+++ test/example.vue
@@ -0,0 +1,25 @@
+<template lang="">
+<div class="">
+  <p></p>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="">
+.hello {
+  color: #9c053c
+}
+
+#world {
+  font-size: 30px
+}
+</style>
